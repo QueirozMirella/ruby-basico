e96ea3a4b543e083ca3a37d6c7b825287132b751
mirellaqueiroz <mirellaqueiroz@campos.rj.gov.br>
2020-01-07 10:35:12 -0300

    módulo 3

---
+++ tipo.rb
@@ -0,0 +1,15 @@
+# no ruby eu nao tenho booleanos. o true e false são objetos.
+
+#fixnum é de numeros inteiros
+#bignum é para numeros inteiros muito grandes
+#eu tenho a possibilidade de escrever binario, só preciso colocar 0b antes do numero
+#se eu quiser um numero em octal, é só escrever 0o antes do numero
+#para escrever em hexadecimal eu coloco 0x antes do numero
+#numero com casas decimais são chamados de float
+
+#strings podem ser utilizadas com aspas simples e aspas duplas, porém com aspas simpes eu não consigo fazer interpolação de strings
+
+#downcase deixa a string toda em minusculo
+#upcase deixa a string toda em maiusculo
+#o método strip remove os espaços em branco do começo e final da string
+#o método capitalize deixa a primeira letra da string em maiusculo
